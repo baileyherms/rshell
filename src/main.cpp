00fcd054d38222204914769a97cf838d5dd77ea4
Bailey Herms <bherm001@ucr.edu>
2015-04-11 01:37:59 -0700

    added simple code to main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,13 @@
 //main file for rshell
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	while(1)
+	{
+		cout << "$ " << endl;
+	}
+	
+	return 0;//will return true of false?
+}
