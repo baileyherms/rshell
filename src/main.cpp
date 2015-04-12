f95f0f09b0f7af4b36edbe7578d0aa1657f495a6
Bailey Herms <bherm001@ucr.edu>
2015-04-11 19:10:46 -0700

    Added $ output to main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,31 @@
 //main file for rshell
 #include <iostream>
+#include <string>
+#include <boost/tokenizer.hpp>
+
 using namespace std;
+using namespace boost;
 
 int main(int argc, char *argv[])
 {
+
 	while(1)
 	{
-		cout << "$ " << endl;
+		int max_input = 1000; //change later
+		char usr_input[max_input];
+	
+		int hold = 0;
+		
+		cout << "$";
+		cin.getline(usr_input, max_input);
+
+
+		while(hold < max_input)
+		{
+			//check the command line argument
+			hold++;
+		}
 	}
 	
-	return 0;//will return true of false?
+	return 0;//will return false
 }
