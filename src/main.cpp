103a5a66eb95babd52c93abf32e373b0d6d861c3
Bailey Herms <bherm001@ucr.edu>
2015-04-18 22:54:23 -0700

    Still have issues with repition and connectors

--- src/main.cpp
+++ src/main.cpp
@@ -1,131 +1,171 @@
 //main file for rshell
 #include <iostream>
 #include <string>
+#include <string.h>
 #include <boost/tokenizer.hpp>
 #include <vector>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 using namespace std;
 using namespace boost;
 
 //make functions
 
+
+
+void get_input(string usr_input)
+{
+		char arr[10000];//tokenize
+		//put input into array
+		for(int i = 0; i < usr_input.size(); i++)
+		{
+			arr[i] = usr_input[i];
+		}
+
+		char* argv[10000];
+		char* tok = strtok(arr, " ");
+		char* argu[10000];
+		char* conn[10000];
+		int x = 0;
+		while(tok != NULL)
+		{
+			argv[x] = tok;
+			x++;
+			tok = strtok(NULL, " ");
+		}
+		argv[x] = '\0';
+		int con_amount = 0;
+		for(int i = 0; i < x; i++)
+		{
+			if(argv[i] == "&&" || argv[i] == "||" || argv[i] == ";")
+			{
+				conn[con_amount] = argv[i];
+				con_amount++;
+			}
+			else if(argv[i] == "#")
+			{
+				break;
+			}
+			else
+			{
+				argu[i] = argv[i];
+			}
+		}
+
+		int argu_amt = 0;
+		bool working = true; //check if command is working
+		bool com_end = false;
+		for(int i = 0; i < x; i++)
+		{
+			
+			//cout << argu[i] << endl;
+			if(argu[i] == "&&" || argu[i] == "||" || argu[i] == ";")
+			{
+				if(argu[i] == "&&")
+				{
+					if(!working)
+					{
+						com_end = true;
+						break;
+					}
+				}
+				else if(argu[i] == "||")
+				{
+					if(working)
+					{
+						com_end = true;
+						break;
+					}
+				}
+				else if(argu[i] == ";")
+				{
+
+				}
+
+			}
+			else if(argu[i] == "#")
+			{
+				working = false;
+				com_end = true;
+				break;
+			}
+			else if(!com_end)
+			{
+				//cout << "Here" << endl;
+				char* run[10000];
+				int y = 0;
+				int w = 0;//separate commands
+				while(argu[w] != "&&" || argu[w] != "||" || argu[w] != "#" || argu[w] != ";" || argu[w] != NULL) 
+				{
+					if(w > x)
+					{
+						break;
+					}
+					run[y] = argu[y];
+					y++;
+					w++;
+				//	cout << "stuck" << endl;
+				}
+				int pid = fork();
+				//cout << "now" << endl;
+				if(pid == -1)
+				{
+					perror("fork");
+					exit(1);
+				}
+				else if(pid == 0)
+				{
+					//cout << run[0] << endl;
+					//cout << endl;
+					if(execvp(run[0], run) == -1)
+					{
+						perror("execvp");
+						exit(1);
+					}
+					/*
+					working = true;
+					char host[255];
+					string login = getlogin();
+					gethostname(host, 255);
+					cout << login << "@" << host << " ";
+					string usr_input;
+					cout << "$";
+					getline(cin, usr_input);
+					*/
+				}
+				else if(pid > 0)
+				{
+					if(-1 == wait(0))
+					{
+						perror("wait");
+						working = false;
+					}
+				}
+			}
+		}
+}
+
+void output()
+{
+	char host[255];
+	string login = getlogin();
+	gethostname(host, 255);
+	cout << login << "@" << host << " ";
+	string usr_input;
+	cout << "$";
+	getline(cin, usr_input);
+	get_input(usr_input);
+}
+
 int main(int argc, char *argv[])
 {
 	while(1)
 	{
-		//int max_input = 10000; //change later
-		string usr_input;
-		vector<string> cmd_arg;//user arguments after token separation
-		int hold = 0;
-		
-		cout << "$";
-		getline(cin, usr_input);
-		unsigned int input_size = usr_input.size() * 2;//just in case
-
-
-		while(hold < usr_input.size())
-		{
-			//check the command line argument
-			hold++;
-		}
-
-		char_separator<char> cmd_tok(" ", "; #");//add other tokens
-		tokenizer<char_separator<char>> mytok(usr_input, cmd_tok);
-		
-		for(auto it = mytok.begin(); it != mytok.end(); ++it)
-		{
-			if(*it == "&&")
-			{
-				cmd_arg.push_back("&&");
-			}
-			else if(*it == "||")
-			{
-				cmd_arg.push_back("||");
-			}
-			else if(*it == ";")
-			{
-				cmd_arg.push_back(";");
-			}
-			else if(*it == "#")
-			{
-				cmd_arg.push_back("#");
-				break;
-			}
-			else
-			{
-				cmd_arg.push_back(*it);
-			}
-		}
-		cmd_arg.push_back("#");
-		vector<string> exec;
-		vector<string> arg;
-		vector<string> con;
-		int traverse = 0;
-		while(cmd_arg[traverse] != "#")
-		{
-			exec.push_back(cmd_arg[traverse]);
-			traverse++;
-			if(!(cmd_arg[traverse] == "&&" || cmd_arg[traverse] == "||" || cmd_arg[traverse] == ";" || cmd_arg[traverse] == "#"))
-			{
-				arg.push_back(cmd_arg[traverse]);
-				//argument
-				traverse++;
-				if(cmd_arg[traverse] == "&&")
-				{
-					con.push_back(cmd_arg[traverse]);
-					//command
-					traverse++;
-				}
-				else if(cmd_arg[traverse] == "||")
-				{
-					con.push_back(cmd_arg[traverse]);
-					//command
-					traverse++;
-				}
-				else if(cmd_arg[traverse] == ";")
-				{
-					con.push_back(cmd_arg[traverse]);
-					//command
-					traverse++;
-				}
-				else if(cmd_arg[traverse] == "#")
-				{
-					break;
-				}
-				else
-				{
-					//error
-					break;
-				}
-			}
-			else if(cmd_arg[traverse] == "&&")
-			{
-				con.push_back(cmd_arg[traverse]);
-				//command
-				traverse++;
-			}
-			else if(cmd_arg[traverse] == "||")
-			{
-				con.push_back(cmd_arg[traverse]);
-				//command
-				traverse++;
-			}
-			else if(cmd_arg[traverse] == ";")
-			{
-				con.push_back(cmd_arg[traverse]);
-				//command
-				traverse++;
-			}
-			else if(cmd_arg[traverse] == "#")
-			{
-				break;
-			}
-			else
-			{
-				//error
-				break;
-			}
-		}
+		output();
+		//get_input();
 	}
 	return 0;
 }
