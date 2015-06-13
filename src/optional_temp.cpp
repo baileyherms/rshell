4526b4280e82f50313e476bc3f1a3f3f87e0ebeb
Bailey Herms <bherm001@ucr.edu>
2015-06-12 21:28:55 -0700

    Fixed optional_temp.cpp

--- src/optional_temp.cpp
+++ src/optional_temp.cpp
@@ -9,20 +9,25 @@ boost::optional<double> degrees()
 {
 	if(deg)
 	{
-		cout << "There is a temperature reported." << endl;
-		cout << deg << " degrees" << endl;
-		return optional<double>{};
+		return deg;
 	}
 	else
 	{
-		cout << "There is no temperature reported." << endl;
 		return optional<double>{};
 	}
 }
 int main()
 {
 	degrees();
+	if(deg)
+		cout << "The temperature is " << deg << " degrees." << endl;
+	else
+		cout << "There is no temperature reported." << endl;
 	deg = 43;
 	degrees();
+	if(deg)
+		cout << "The temperature is " << deg << " degrees." << endl;
+	else
+		cout << "There is no temperature reported." << endl;
 	return 0;
 }
